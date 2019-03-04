59d328c1fad821b6761ca101aa5db046428cc97e
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-03-05 00:51:19 +0300

    edit signup component

--- src/components/Auth/signup/signupForm.js
+++ src/components/Auth/signup/signupForm.js
@@ -1,28 +1,38 @@
 import React from 'react';
+import '../../../styles/signup.css';
 
 const SignupForm = ({handleChange, state, handleSubmit}) => (
-    <div className='offset-md-4 col-md-4'>
-        <form onSubmit={ handleSubmit }>
-            <div className={'text-center text-muted'}><h2>SignupForm</h2></div>
-            <hr/>
-            <div className="form-group">
-                <label htmlFor="email">Email address:</label>
-                <input type="email" className="form-control" id="email" onChange= {handleChange} />
-                <span className={'text-danger'}>{state.emailError}</span>
+    <div className="signupFormWrapper">
+        <div className='row innerSignuwrapper'>
+            <div className='col-md-8'>
+                <img src={require("../../../static/images/Carpool.jpg")} alt='signupImage' className='img-fluid imageClass'/>
             </div>
-            <div className="form-group">    
-                <label htmlFor="password">Password:</label>
-                <input type="password" className="form-control" id="password" onChange={handleChange}/>
-                <span className={'text-danger'}>{state.passwordError}</span>    
+            <div className='offset-md-0 col-md-4 formColumn'>
+                <form onSubmit={ handleSubmit }>
+                    <div className={'text-center text-muted'}><h2>Create Account</h2></div>
+                    <hr/>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" className="form-control" id="email" onChange= {handleChange} />
+                        <span className={'text-danger'}>{state.emailError}</span>
+                    </div>
+                    <div className="form-group">    
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" className="form-control" id="password" onChange={handleChange}/>
+                        <span className={'text-danger'}>{state.passwordError}</span>    
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="confirmPass">Coonfirm password:</label>
+                        <input type="password" className="form-control" id="confirmPass" onChange={handleChange}/>
+                        <span className={'text-danger'}>{state.confirmPassError}</span>
+                    </div>
+                    <button type="submit" className="btn btn-success btn-block" disabled={state.confirmPassHasError || state.passwordHasError || state.emailHasError}>Submit</button>
+                </form>
             </div>
-            <div className="form-group">
-                <label htmlFor="confirmPass">Coonfirm password:</label>
-                <input type="password" className="form-control" id="confirmPass" onChange={handleChange}/>
-                <span className={'text-danger'}>{state.confirmPassError}</span>
-            </div>
-            <button type="submit" className="btn btn-primary" disabled={state.confirmPassHasError || state.passwordHasError || state.emailHasError}>Submit</button>
-        </form>
+            
+        </div>
     </div>
 )
+
 
 export default SignupForm;
